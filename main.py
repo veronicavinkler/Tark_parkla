a0ae06c789c41ee38f5d7c1445b5c5cf25db23f9
veronicavinkler <161318939+veronicavinkler@users.noreply.github.com>
2024-11-27 22:29:41 +0200

    nr1_27.11.2024

---
+++ main.py
@@ -0,0 +1,25 @@
+import kivy
+kivy.require('2.3.0')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+
+#MATERJALID
+#https://www.techwithtim.net/tutorials/python-module-walk-throughs/kivy-tutorial/object-properties
+#https://www.geeksforgeeks.org/python-adding-image-in-kivy-using-kv-file/
+#https://kivy.org/doc/stable/guide/packaging.html
+#https://www.tutorialspoint.com/kivy/kivy-images.htm
+#https://kivy.org/doc/stable/api-kivy.uix.label.html
+#https://kivy.org/doc/stable/api-kivy.core.text.html
+
+
+class My(Widget):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return My()
+
+if __name__ == '__main__':
+    MyApp().run()
