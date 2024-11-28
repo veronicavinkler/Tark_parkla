a075ef1524aea7f8f693e63bfef3a86155778166
veronicavinkler <161318939+veronicavinkler@users.noreply.github.com>
2024-11-28 10:46:13 +0200

    Update tarkparkla.py
    koodi jupp kirja

--- tarkparkla.py
+++ tarkparkla.py
@@ -1,1 +1,16 @@
 
+if __name=="__main__":
+  while(True):
+    t1=threading.Thread(target=parkla, args=(1, )
+    t2=threading.Thread(target=parkla, args=(2, )
+    t3=threading.Thread(target=parkla, args=(3, )
+
+    t1.start()
+    time.sleep(1)#et oleks lihtsam logi lugeda
+    t2.start()
+    time.sleep(2)#et oleks lihtsam logi lugeda
+    t3.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
