4182b2865e58c559b0893d5423450a883abd869c
veronicavinkler <161318939+veronicavinkler@users.noreply.github.com>
2024-12-19 18:30:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -28,37 +28,54 @@ from time import *
 from time import sleep
 import threading
 import time
+#WINDOW
+from kivy.config import Config
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '1024')
 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
 
+#WIDGETS
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import ScrollView
 
-#IMPORTANT STUFF
-#GPIO.setwarnings(False)
+#ADDITION
+from RPi import GPIO
+from kivy.clock import Clock
+from random import randint
+from time import *
+from time import sleep
+import time
+import threading
 
-#SETUP
-#ledred = [5, 22, 27]
-#ledgreen = [26, 19, 13]
-#TRIG = [24, 23, 18]
-#ECHO = [21, 20, 16]
-
-speed_of_sound = 343.0
-
-
-print("Distance in measurement")
-
-#GPIO.setmode(GPIO.BMC)
-#GPIO.setup(ledred, GPIO.OUT)
-#GPIO.setup(ledgreen, GPIO.OUT)
-#GPIO.setup(TRIG, GPIO.OUT)
-#GPIO.setup(ECHO, GPIO.IN)
-
-#import I2C_LCD_driver
-#mylcd = I2C_LCD_driver.lcd()
+GPIO.setwarnings(False) #set no false alarms
+print("Distants mootmisel...") #our intro
+import I2C_LCD_driver
 
 class ParklaApp(App):
-    def build(self):
+    #DEFINING
+    ps = [False, False, False]
+    ledred = [5, 22, 27]
+    ledgreen = [26, 19, 13]
+    TRIG = [24, 23, 18]
+    ECHO = [21, 20, 16]
+    speed_of_sound = 343.0 #because we don't know that
+    
+    def Initializing(self, ledred, ledgreen, TRIG, ECHO):
+        #SETUP
+        GPIO.setmode(GPIO.BMC)
+        GPIO.setup(ledred, GPIO.OUT)
+        GPIO.setup(ledgreen, GPIO.OUT)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
+
+    def build(self):        
+        Initializing()
         self.flo = FloatLayout()
 
-        # Background Image
+        # BACKGROUND IMAGE BEING SET
         self.flo.add_widget(
             Image(
                 source = 'C:/Users/veron/Ajaplaneerimine/back_2.png',
@@ -71,9 +88,10 @@ class ParklaApp(App):
                 size_hint = (0.6, 1),
                 pos_hint = {"x": 0, "y": 0},
                 allow_stretch = True))
-        # Log Section
+        
+        #LOGI TAUSTA LISAMINE
         self.log_messages = []
-        self.log_label = Label(
+        self.log_label = Label( #LOG PEALKIRI
             text="Log sättib valmis...\n",
             halign='left',
             valign='top',
@@ -82,17 +100,17 @@ class ParklaApp(App):
         )
         self.log_label.bind(texture_size=self.log_label.setter('size'))
 
-        log_scroll = ScrollView(
+        log_scroll = ScrollView( #SCROLLAMISE OSA LOGILE
             size_hint=(0.4, 0.6),
             pos_hint={'x': 0.55, 'y': 0.3},
             do_scroll_x=False
         )
-        log_scroll.add_widget(self.log_label)
+        log_scroll.add_widget(self.log_label)#Lisame ekraanile
         self.flo.add_widget(log_scroll)
         
         #MINI SETUP
         self.Parking_spaces = [False, False, False]
-        self.Pi = [
+        self.Pilt = [
             'C:/Users/veron/Ajaplaneerimine/blackcar.png',
             'C:/Users/veron/Ajaplaneerimine/blackcar.png',
             'C:/Users/veron/Ajaplaneerimine/blackcar.png'
@@ -101,64 +119,64 @@ class ParklaApp(App):
         self.PL = []
         for i in range(3):
             parking_lot = Image(
-                source = self.Pi[i],
+                source = self.Pilt[i],
                 size_hint = (0.3, 0.3),
                 pos_hint = {"x": (i - 0.26) * 0.2, "y": 0.6},
                 allow_stretch = True
             )
             self.flo.add_widget(parking_lot)
             self.PL.append(parking_lot)
-        #BUTTONS
-        for i in range(3):
-            green_b = Button(
-                background_normal = 'C:/Users/veron/Ajaplaneerimine/greenbutton.png',
-                size_hint = (0.1, 0.1),
-                pos_hint = {"x": 0.05 + i * 0.2, "y": 0.5},
-            )
-            self.flo.add_widget(green_b)
-            red_b = Button(
-                background_normal = 'C:/Users/veron/Ajaplaneerimine/redbutton.png',
-                size_hint = (0.1, 0.1),
-                pos_hint = {"x": 0.05 + i * 0.2, "y": 0.35},
-            )
-            self.flo.add_widget(red_b)
-            black_b = Button(
-                background_normal = 'C:/Users/veron/Ajaplaneerimine/blackbutton.png',
-                size_hint = (0.1, 0.1),
-                pos_hint = {"x": 0.05 + i * 0.2, "y": 0.2},
-            )
-            self.flo.add_widget(black_b)
 
-
+        
+        t1 = threading.Thread(target=self.mp, args=(1, TRIG[0], ECHO[0]))
+        t2 = threading.Thread(target=self.mp, args=(2, TRIG[1], ECHO[1]))
+        t3 = threading.Thread(target=self.mp, args=(3, TRIG[2], ECHO[2]))
+        
+        t1.start()
+        t2.start()
+        t3.start()
+        
+        Fps = 0 #vabad parkimise kohad
         for i in range(3):
-            threading.Thread(target=self.mp, args=(i,)).start()
-            #Free_p_s_string = str(Free_parking_spaces) + " Parking spaces is free"
-            #mylcd.lcd_display_string(Free_p_s_string, 1)
-            #GPIO.cleanup()        
+            if ps == True:
+                Fps += 1
+            i += 1
+        
+        Fps_s = str(Fps) + " Parking spaces is free"
+        mylcd.lcd_display_string(Fps_s, 1)
+        GPIO.cleanup()        
+        self.add_to_log(Fps_s)#lisame logisse
+        
         return self.flo
         
-    def mp (self, space_num):
+    def mp (self, space_num, trig, echo, ledred, ledgreen):
         while(True):
-            #setup
-            #GPIO.output(trig, False)
-            #print("waiting ", self.space_num, "\n")
+            #KAUGUSE MOOTMINE
+            GPIO.output(trig, False)
+            m = "waiting ", self.space_num, "\n"
+            print(m)
+            self.add_to_log(m) #lisame logisse
             
-            #GPIO.output(trig, True)
-            #time.sleep(0.00001)
-            #GPIO.output(trig, False)
+            GPIO.output(trig, True)
+            time.sleep(0.00001)
+            GPIO.output(trig, False)
 
-            #while GPIO.input(echo)==0:
-            #    pulse_start=time()
-            #while GPIO.input(echo)==1:
-            #    pulse_end=time.time()
-
-            #pulse_duration = pulse_end-pulse_start
-            #distance=(pulse_duration*speed_of_sound)/2
-            #distance = round(distance*100)/2
-            print("Ootan vastust... ")
-            distance = randint(0, 30)
+            while GPIO.input(echo)==0:
+                pulse_start=time()
+            while GPIO.input(echo)==1:
+                pulse_end=time.time()
+            
+            #MOOTMISTE ARVUTAMINE
+            pulse_duration = pulse_end-pulse_start
+            distance=(pulse_duration*speed_of_sound)/2
+            distance = round(distance*100)/2
+            
+            m = f"Ootan vastust parkimise kohalt {space_num}"
+            print(m) #vastus konsooli
+            self.add_to_log(m)
+            
             status = self.p_checking(distance)
-            Clock.schedule_once(lambda dt: self.ubuntu(space_num, status, distance))
+            Clock.schedule_once(lambda dt: self.ubuntu(space_num, status, distance, ledred[space_num-1], ledgreen[space_num-1]))
             time.sleep(2)
 
     def p_checking(self, distance):
@@ -170,14 +188,14 @@ class ParklaApp(App):
             return "vaba"
         return "lol wtf"
 
-    def ubuntu(self, space_num, status, distance):
+    def ubuntu(self, space_num, status, distance, ledred, ledgreen):
         if status == "viga":
             m = f"Parkimise kohal {space_num + 1} in VIGA"
             self.PL[space_num].source = 'C:/Users/veron/Ajaplaneerimine/blackcar.png'
-            #mylcd_display_string(f"Viga kohal{space_num + 1}")
+            mylcd_display_string(f"Viga kohal{space_num + 1}")
             for i in range(5):
-                #GPIO.output(led_r, True)
-                #GPIO.output(led_g, True)
+                GPIO.output(led_r, True)
+                GPIO.output(led_g, True)
                 i=i+1
         elif status == "voetud":
             m = f"Parkimise koht {space_num + 1} on võetud"
